fcb2cb7d4b0d182f94d6305e8179201e64a78379
Ariel Hicks <hicks.ariel@gmail.com>
2019-02-25 01:14:42 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,29 @@
 class BankAccount
 
+  attr_accessor :balance, :status, :amount
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = 'open'
+  end
+
+  def deposit(deposit_amount)
+    self.balance += deposit_amount
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @status == 'open' && @balance > 0
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,21 @@
 class Transfer
-  # your code here
+    # your code here
+    attr_reader :amount, :sender, :receiver
+    attr_accessor :status
+
+    def initialize(sender, receiver, amount)
+      @sender = sender
+      @receiver = receiver
+      @amount = amount
+      @status = 'pending'
+  end
+
+    def valid?
+      @sender.valid? && @receiver.valid?
+    end
+
+    def execute_transaction
+      
+    end
+
 end
